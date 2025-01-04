23a0a37872aa72f70836866a14a7a63d531151c2
my_linux <2115653184@qq.com>
2025-01-04 11:09:25 +0800

    i have finished memory calculation

--- code/load.asm
+++ code/load.asm
@@ -1,6 +1,6 @@
 %include "boot.inc"
-section load vstart = 0x900
-jmp loader_start 
+section load vstart=0x900
+jmp detected_memory
 GDT_ADDR:
     GDT_BASE:
         dd 0x00000000
@@ -17,6 +17,48 @@ GDT_ADDR:
     gdt_ptr:
         dw $ - GDT_ADDR - 1
         dd GDT_ADDR
+    ards_buf times 254 db 0 
+    ards_count dd 0 
+detected_memory:
+    xor ebx, ebx 
+    xor eax, eax
+    mov es, ax 
+    mov edi, ards_buf
+    mov ecx, 20
+    mov edx, 0x534d4150 
+    .probe_loop:
+        mov eax, 0xe820
+        int 0x15
+        jc .probe_end
+        cmp ebx, 0
+        je .probe_end
+        add di, cx
+        inc dword [es:ards_count]
+        loop .probe_loop
+    .probe_end:
+    .culculate_memory:
+        mov word cx, [es:ards_count]
+        mov edi, ards_buf
+        xor eax, eax
+        push eax
+        .cmp_loop: 
+            mov eax, [es:edi]
+            mov edx, [es:edi + 8]
+            add eax, edx
+            pop edx
+            push edx
+            cmp eax, edx
+            jg .push_eax
+            jle .next
+            .push_eax: 
+                push eax
+            .next:
+                add edi, 20 
+                loop .cmp_loop
+            pop eax 
+
+            
+            
 loader_start:
     in al, 0x92
     or al, 2
