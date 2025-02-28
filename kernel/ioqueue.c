d324d0ea9c0de5b51d138772856307e164f2b0a8
my_linux <2115653184@qq.com>
2025-02-28 11:46:45 +0800

    finish ioqueue

--- kernel/ioqueue.c
+++ kernel/ioqueue.c
@@ -2,12 +2,56 @@
 void ioqueue_init(struct ioqueue *ioqueue){
     memset(ioqueue->buf, 0, BUF_SIZE);
     ioqueue->consumer = NULL;
-    ioqueue->productor = NULL;
+    ioqueue->producer = NULL;
     lock_init(&ioqueue->mutex);
     ioqueue->head = 0; 
     ioqueue->tail = 0; 
+}
+int32_t next_pos(int32_t pos){
+    return (pos + 1) % BUF_SIZE;
+}
+static bool ioq_is_empty(struct ioqueue *i){
+    assert(!is_enable_interrupts());
+    return i->head == i->tail;
+}
+static bool ioq_is_full(struct ioqueue *i){
+    assert(!is_enable_interrupts());
+    return next_pos(i->head) == i->tail;
+}
+static void ioq_wait(struct task_struct **waiter){
+    assert(waiter != NULL && *waiter == NULL);
+    *waiter = running_thread();
+    thread_block(TASK_BLOCKED);
+}
+static void ioq_wakeup(struct task_struct **waiter){
+    assert(waiter != NULL && *waiter != NULL);
+    thread_unblock(*waiter);
+    *waiter = NULL;
 }
 void is_empty(struct ioqueue *i){
     lock_acquire(&i->mutex);
     
 } 
+char ioq_getchar(struct ioqueue *i){
+    assert(!is_enable_interrupts());
+    while(ioq_is_empty(i)){
+        lock_acquire(&i->mutex);
+        ioq_wait(&i->consumer);
+        lock_release(&i->mutex);
+    }
+    char c = i->buf[i->tail];
+    i->tail = next_pos(i->tail);
+    if(i->producer != NULL) ioq_wakeup(&i->producer);
+    return c;
+}
+void ioq_putchar(char c, struct ioqueue *i){
+    assert(!is_enable_interrupts());
+    while(ioq_is_full(i)){
+        lock_acquire(&i->mutex);
+        ioq_wait(&i->producer);
+        lock_release(&i->mutex);
+    }
+    i->buf[i->head] = c;
+    i->head = next_pos(i->head);
+    if(i->consumer != NULL) ioq_wakeup(i->consumer);
+}
--- kernel/ioqueue.h
+++ kernel/ioqueue.h
@@ -1,12 +1,16 @@
 #include "thread.h"
 #include "semaphore.h"
 #define BUF_SIZE 64
-void ioqueue_init();
-void is_empty();
-void is_full();
-char peek(); 
-static struct ioqueue{
-    struct task_struct *productor;
+void ioq_init();
+static bool ioq_is_empty(struct ioqueue *i);
+static bool ioq_is_full(struct ioqueue *i);
+static int32_t next_pos(int32_t pos);
+static void ioq_wait(struct task_struct **waiter);
+static void ioq_wakeup(struct task_struct ** wairer);
+char ioq_getchar(struct ioqueue *i);
+void ioq_putchar(char c, struct ioqueue *i);
+struct ioqueue{
+    struct task_struct *producer;
     struct task_struct *consumer;
     struct lock mutex;
     char buf[BUF_SIZE];
